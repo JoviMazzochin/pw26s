70d8e0f54795aff398804f94007b4dae23ef4501
João Mazzochin <joaomazzochin@Joaos-MacBook-Air.local>
2021-10-07 22:04:19 -0300

    Repositorys implemented 2

--- src/main/java/br/utfpr/edu/pw26s/repository/FuncionarioRepository.java
+++ src/main/java/br/utfpr/edu/pw26s/repository/FuncionarioRepository.java
@@ -1,2 +1,7 @@
-package br.utfpr.edu.pw26s.repository;public interface FuncionarioRepository {
+package br.utfpr.edu.pw26s.repository;
+
+import br.utfpr.edu.pw26s.model.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
 }
